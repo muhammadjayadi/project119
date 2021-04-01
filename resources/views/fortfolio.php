2372b95c41590bbb5446c6508630479ac0d0935a
jayadi <muhammadjayadi754@gmail.com>
2021-04-01 20:39:08 +0800

    v1_fortfolio

---
+++ resources/views/fortfolio.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>fortfolio</title>
+  </head>
+  <body>
+  <!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>portfolio</title>
+</head>
+<style>
+    .navbar {
+        background-color: #26acd9;
+    }
+
+    .jumbotron {
+        background-color: #26acd9;
+    }
+
+    img {
+        /* margin-bottom: 30px; */
+        margin: 10px 20px 30px 30px;
+    }
+
+    h2 {
+        margin: 10px 20px 30px 30px;
+    }
+
+</style>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark shadow">
+        <div class="container">
+            <a class="navbar-brand" href="">Data Diri</a>
+            <div class="navbar-nav ml-auto">
+
+                <form class="form-inline my-2 my-lg-0 ">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+                </form>
+
+            </div>
+
+        </div>
+    </nav>
+    <div class="jumbotron ">
+
+    </div>
+    <div class="container   " style="margin-top: -80px; ">
+        <div class="card " >
+            <div class="card-body "></div>
+            <div class="row align-items-center">
+                <div class="col-2 ">
+                    <img src="/asset/jayadi.jpg" class="img-fluid rounded-circle" alt="">
+
+                </div>
+                <div class="col-10">
+
+                    <h2>Muhammad Jayadi</h2>
+                </div>
+                <div class="col-2">
+
+                    <h2></h2>
+                </div>
+            </div>
+
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+
+                <table class="table ">
+                    <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th>Identitas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">1</td>
+                            <td>Nama</td>
+                            <td>Muhammad Jayadi</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">2</td>
+                            <td>Nim</td>
+                            <td>1810530119</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">3</td>
+                            <td>jurusan</td>
+                            <td>Ilmu komputer</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">4</td>
+                            <td>perguruan tinggi</td>
+                            <td>universitas Bumigora</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">5</td>
+                            <td>Kelas</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-4">
+                <p class="lead text-center">
+                    RPL <br>
+                    Rekayasa Perangkat Lunak
+                </p>
+            </div>
+
+        </div><br>
+
+        <h5 style="text-align: center;">Progres</h5>
+        <h5>HTML</h5>
+        <div class="progress">
+
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="50" aria-valuemax="100" style="width: 50% ;">  </div>
+        </div><br>
+        <h5>CSS</h5>
+        <div class="progress">
+
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="50" aria-valuemax="100" style="width: 50% ;">  </div>
+        </div><br>
+        <h5>JavaScript</h5>
+        <div class="progress">
+
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="30" aria-valuemax="100" style="width: 30% ;"> </div>
+        </div><br>
+        <h5>PHP</h5>
+        <div class="progress">
+
+            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="40" aria-valuemax="100" style="width: 40% ;"> </div>
+        </div>
+
+    </div>
+    </div>
+    </div><br>
+    <footer style=" background-color: #2680d9; color:#ecf0f1d9"   class="text-center p-4" >
+      
+    
+        <p>CopyRigh &copy Jayadi2020</p>
+    </footer>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
+
+
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fortfolio', function () {
+    return view('fortfolio');
+});
